505d4bc9ff545b51907d79a0ba56f4e1a75c53a6
Matthew Smith <matthew.smith2@extendhealth.com>
2011-11-02 10:44:30 -0600

    Added input stream overloads for wma split and combine methods.

--- yeti/wma/WmaFunctions.cs
+++ yeti/wma/WmaFunctions.cs
@@ -8,8 +8,8 @@ namespace yeti.wma
         /// <summary>
         /// Combines files into one in the order they are passed in..
         /// </summary>
-        /// <param name="filepaths">The filepaths to combine</param>
-        /// <param name="outputFile">The filepath to save the combined file to.</param>
+        /// <param name="filepaths">The file paths to combine</param>
+        /// <param name="outputFile">The file path to save the combined file to.</param>
         public static void Combine(string outputFile, params string[] filepaths)
         {
             Combine(outputFile, 1, filepaths);
@@ -18,9 +18,9 @@ namespace yeti.wma
         /// <summary>
         /// Combines files into one in the order they are passed in..
         /// </summary>
-        /// <param name="bufferMultiplier">The multipler to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
-        /// <param name="filepaths">The filepaths to combine</param>
-        /// <param name="outputFile">The filepath to save the combined file to.</param>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        /// <param name="filepaths">The file paths to combine</param>
+        /// <param name="outputFile">The file path to save the combined file to.</param>
         public static void Combine(string outputFile, int bufferMultiplier, params string[] filepaths)
         {
             Combine(new FileStream(outputFile, FileMode.Create), bufferMultiplier, filepaths);
@@ -29,7 +29,28 @@ namespace yeti.wma
         /// <summary>
         /// Combines files into one in the order they are passed in..
         /// </summary>
-        /// <param name="filepaths">The filepaths to combine</param>
+        /// <param name="inputStreams">The streams to combine</param>
+        /// <param name="outputFile">The file path to save the combined file to.</param>
+        public static void Combine(string outputFile, params Stream[] inputStreams)
+        {
+            Combine(new FileStream(outputFile, FileMode.Create), 1, inputStreams);
+        }
+
+        /// <summary>
+        /// Combines files into one in the order they are passed in..
+        /// </summary>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        /// <param name="inputStreams">The streams to combine</param>
+        /// <param name="outputFile">The file path to save the combined file to.</param>
+        public static void Combine(string outputFile, int bufferMultiplier, params Stream[] inputStreams)
+        {
+            Combine(new FileStream(outputFile, FileMode.Create), bufferMultiplier, inputStreams);
+        }
+
+        /// <summary>
+        /// Combines files into one in the order they are passed in..
+        /// </summary>
+        /// <param name="filepaths">The file paths to combine</param>
         /// <param name="outputStream">The stream to save the combined file to.</param>
         public static void Combine(Stream outputStream, params string[] filepaths)
         {
@@ -39,31 +60,49 @@ namespace yeti.wma
         /// <summary>
         /// Combines files into one in the order they are passed in..
         /// </summary>
-        /// <param name="bufferMultiplier">The multipler to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
-        /// <param name="filepaths">The filepaths to combine</param>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        /// <param name="filepaths">The file paths to combine</param>
         /// <param name="outputStream">The stream to save the combined file to.</param>
         public static void Combine(Stream outputStream, int bufferMultiplier, params string[] filepaths)
         {
-            if (filepaths.Length > 0)
-            {
-                var wmaInput = new WmaStream(filepaths[0]);
-                var wmaOutput = new WmaWriter(outputStream,
-                    wmaInput.Format,
-                    wmaInput.Profile);
-                var buffer = new byte[wmaOutput.OptimalBufferSize * bufferMultiplier];
+            var inputStreams = new Stream[filepaths.Length];
 
-                try
+            for (var i = 0; i < filepaths.Length; i++)
+            {
+                inputStreams[i] = new WmaStream(filepaths[i]);
+            }
+
+            Combine(outputStream, bufferMultiplier, inputStreams);
+        }
+
+        /// <summary>
+        /// Combines files into one in the order they are passed in..
+        /// </summary>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        /// <param name="inputStreams">The streams to combine</param>
+        /// <param name="outputStream">The stream to save the combined file to.</param>
+        public static void Combine(Stream outputStream, int bufferMultiplier, params Stream[] inputStreams)
+        {
+            if (inputStreams.Length <= 0)
+                return;
+
+            var wmaInput = new WmaStream(inputStreams[0]);
+            var wmaOutput = new WmaWriter(outputStream,
+                                          wmaInput.Format,
+                                          wmaInput.Profile);
+            var buffer = new byte[wmaOutput.OptimalBufferSize * bufferMultiplier];
+
+            try
+            {
+                WriteFile(wmaOutput, wmaInput, buffer);
+                for (var i = 1; i < inputStreams.Length; i++)
                 {
-                    WriteFile(wmaOutput, wmaInput, buffer);
-                    for (int i = 1; i < filepaths.Length; i++)
-                    {
-                        WriteFile(wmaOutput, new WmaStream(filepaths[i]), buffer);
-                    }
+                    WriteFile(wmaOutput, new WmaStream(inputStreams[i]), buffer);
                 }
-                finally
-                {
-                    wmaOutput.Close();
-                }
+            }
+            finally
+            {
+                wmaOutput.Close();
             }
         }
 
@@ -73,7 +112,7 @@ namespace yeti.wma
         /// <param name="inputFile">The input file path.</param>
         /// <param name="outputFile">The output file path.</param>
         /// <param name="startTime">The time that the split from the source file should start.</param>
-        /// <param name="endTime">The time that the split from the source file shound end.</param>
+        /// <param name="endTime">The time that the split from the source file should end.</param>
         public static void Split(string inputFile, string outputFile, TimeSpan startTime, TimeSpan endTime)
         {
             Split(inputFile, outputFile, startTime, endTime, 1);
@@ -85,10 +124,9 @@ namespace yeti.wma
         /// <param name="inputFile">The input file path.</param>
         /// <param name="outputFile">The output file path.</param>
         /// <param name="startTime">The time that the split from the source file should start.</param>
-        /// <param name="endTime">The time that the split from the source file shound end.</param>
-        /// <param name="bufferMultiplier">The multipler to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
-        public static void Split(
-            string inputFile, string outputFile, TimeSpan startTime, TimeSpan endTime, int bufferMultiplier)
+        /// <param name="endTime">The time that the split from the source file should end.</param>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        public static void Split(string inputFile, string outputFile, TimeSpan startTime, TimeSpan endTime, int bufferMultiplier)
         {
             Split(inputFile, new FileStream(outputFile, FileMode.Create), startTime, endTime, bufferMultiplier);
         }
@@ -99,7 +137,7 @@ namespace yeti.wma
         /// <param name="inputFile">The input file path.</param>
         /// <param name="outputStream">The stream to write the split portion of the file to.</param>
         /// <param name="startTime">The time that the split from the source file should start.</param>
-        /// <param name="endTime">The time that the split from the source file shound end.</param>
+        /// <param name="endTime">The time that the split from the source file should end.</param>
         public static void Split(string inputFile, Stream outputStream, TimeSpan startTime, TimeSpan endTime)
         {
             Split(inputFile, outputStream, startTime, endTime, 1);
@@ -111,12 +149,61 @@ namespace yeti.wma
         /// <param name="inputFile">The input file path.</param>
         /// <param name="outputStream">The stream to write the split portion of the file to.</param>
         /// <param name="startTime">The time that the split from the source file should start.</param>
-        /// <param name="endTime">The time that the split from the source file shound end.</param>
-        /// <param name="bufferMultiplier">The multipler to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
-        public static void Split(
-            string inputFile, Stream outputStream, TimeSpan startTime, TimeSpan endTime, int bufferMultiplier)
+        /// <param name="endTime">The time that the split from the source file should end.</param>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        public static void Split(string inputFile, Stream outputStream, TimeSpan startTime, TimeSpan endTime, int bufferMultiplier)
         {
-            var wmaInput = new WmaStream(inputFile);
+            Split(new FileStream(inputFile, FileMode.Create), outputStream, startTime, endTime, bufferMultiplier);
+        }
+
+        /// <summary>
+        /// Cuts out a smaller portion of a Wma file.
+        /// </summary>
+        /// <param name="inputStream">The input stream.</param>
+        /// <param name="outputFile">the output file path.</param>
+        /// <param name="startTime">The time that the split from the source file should start.</param>
+        /// <param name="endTime">The time that the split from the source file should end.</param>
+        public static void Split(Stream inputStream, string outputFile, TimeSpan startTime, TimeSpan endTime)
+        {
+            Split(inputStream, outputFile, startTime, endTime, 1);
+        }
+
+        /// <summary>
+        /// Cuts out a smaller portion of a Wma file.
+        /// </summary>
+        /// <param name="inputStream">The input stream.</param>
+        /// <param name="outputFile">The output file path.</param>
+        /// <param name="startTime">The time that the split from the source file should start.</param>
+        /// <param name="endTime">The time that the split from the source file should end.</param>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        public static void Split(Stream inputStream, string outputFile, TimeSpan startTime, TimeSpan endTime, int bufferMultiplier)
+        {
+            Split(inputStream, new FileStream(outputFile, FileMode.Create), startTime, endTime, bufferMultiplier);
+        }
+
+        /// <summary>
+        /// Cuts out a smaller portion of a Wma file.
+        /// </summary>
+        /// <param name="inputStream">The input stream.</param>
+        /// <param name="outputStream">The stream to write the split portion of the file to.</param>
+        /// <param name="startTime">The time that the split from the source file should start.</param>
+        /// <param name="endTime">The time that the split from the source file should end.</param>
+        public static void Split(Stream inputStream, Stream outputStream, TimeSpan startTime, TimeSpan endTime)
+        {
+            Split(inputStream, outputStream, startTime, endTime, 1);
+        }
+
+        /// <summary>
+        /// Cuts out a smaller portion of a Wma file.
+        /// </summary>
+        /// <param name="inputStream">The input stream.</param>
+        /// <param name="outputStream">The stream to write the split portion to.</param>
+        /// <param name="startTime">The time that the split from the source stream should start.</param>
+        /// <param name="endTime">The time that the split from the source stream should end.</param>
+        /// <param name="bufferMultiplier">The multiplier to use against the OptimalBufferSize of the file for the read buffer, sometimes a larger than optimal buffer size is better.</param>
+        public static void Split(Stream inputStream, Stream outputStream, TimeSpan startTime, TimeSpan endTime, int bufferMultiplier)
+        {
+            var wmaInput = new WmaStream(inputStream);
             var wmaOutput = new WmaWriter(outputStream, wmaInput.Format, wmaInput.Profile);
             int bytesPerSec = wmaInput.Format.nAvgBytesPerSec;
             var stopPosition = (long)(bytesPerSec*endTime.TotalSeconds);
@@ -138,8 +225,7 @@ namespace yeti.wma
             WriteFile(wmaOutput, wmaInput, buffer, -1);
         }
 
-        private static void WriteFile(
-            WmaWriter wmaOutput, WmaStream wmaInput, byte[] buffer, long stopPosition)
+        private static void WriteFile(WmaWriter wmaOutput, WmaStream wmaInput, byte[] buffer, long stopPosition)
         {
             if (stopPosition == -1)
             {
